ae2059c370b7b4d883431f2502de159554408514
Abdul9010150809 <samm41236@gmail.com>
2024-12-13 03:29:13 +0530

    updated status

---
+++ java/JAVA/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello  Sir");
+        System.out.println("Hello  Sir");
+    }
+}
